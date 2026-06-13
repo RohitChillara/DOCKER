04f3d4a5a76ccd085a102f5cf72c7529c5925424
Rohit Chillara <rohitchillara@gmail.com>
2026-06-13 14:07:19 +0200

    Added working FastAPI application

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
 
-#Create an object named app to re-route all the requests from here.
-app=FastAPI()
+
+app=FastAPI() #creating an instance of the FastAPI class, the basic step to start building an API using FastAPI. This instance will be used to define routes and handle requests.
+
+@app.get("/") #decorator to specify the type of request and the path
+def read_root():
+    return {"message": "Hello from Rohit using FastAPI"}
